43eb16bc7f4f3406407efe76f9668801fac95227
Carlos Junior <carlos-junior08@hotmail.com>
2022-06-24 08:19:59 -0300

    Aula 03

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,25 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
+
+// export const client = new ApolloClient({
+//   uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oygo6m186m01xn9b6y27vu/master',
+//   cache: new InMemoryCache()
+// })
+
+const defaultOptions: DefaultOptions = {
+
+
+  watchQuery: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+  query: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+};
 
 export const client = new ApolloClient({
   uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oygo6m186m01xn9b6y27vu/master',
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+  defaultOptions: defaultOptions,
+});
