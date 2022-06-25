31e9423a07c20dde8c6371eb5664dd40b79ab7b9
Carlos Junior <carlos-junior08@hotmail.com>
2022-06-25 14:39:46 -0300

    Aula 04

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -7,7 +7,6 @@ import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
 
 const defaultOptions: DefaultOptions = {
 
-
   watchQuery: {
     fetchPolicy: "no-cache",
     errorPolicy: "ignore",
@@ -19,7 +18,10 @@ const defaultOptions: DefaultOptions = {
 };
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oygo6m186m01xn9b6y27vu/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache(),
   defaultOptions: defaultOptions,
 });
